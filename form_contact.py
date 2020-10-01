cfe546bd27d4ec6c4667f27f8b868e7a4b0a5991
josepmartorell <martorelljosep@gmail.com>
2020-10-01 11:49:43 +0200

    update

--- form_contact.py
+++ form_contact.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect()
 
 class ContactForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired('Name cannot be empty')])
-    email = StringField('E-mail', validators=[DataRequired('Email cannot be empty'), Email('Enter a valid email')])
-    subject = StringField('Subject', validators=[DataRequired('Subject cannot be empty')])
+    subject = StringField('E-mail', validators=[DataRequired('Email cannot be empty'), Email('Enter a valid email')])
+    email = StringField('Subject', validators=[DataRequired('Subject cannot be empty')])
     message = TextAreaField('Message', validators=[DataRequired('Message cannot be empty')])
     submit = SubmitField('SUBMIT')
